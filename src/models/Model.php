edee8c9eaaa66cc4faf4f2f31a99c76923022de0
wellingtonsi <12ribe@gmail.com>
2022-10-09 13:26:44 -0300

    código

---
+++ src/models/Model.php
@@ -0,0 +1,53 @@
+<?php
+
+class Model {
+    protected static $tableName = '';
+    protected static $colums = [];
+    protected $values = [];
+
+    function __construct($arr){
+        $this->loadFromArray($arr);
+    }
+
+    public function loadFromArray($arr){
+        if($arr){
+            foreach($arr as $key => $value){
+                $this->$key = $value;
+            }
+        }
+    }
+
+    public function __get($key){
+        return $this->values[$key];
+    }
+
+    public function __set($key,$value){
+        $this->values[$key] = $value;
+    }
+
+    public static function getSelect($filters = [],$colums = '*'){
+        $sql = "SELECT $colums FROM ".
+                static::$tableName .
+                static::getFilters($filters);
+        return $sql;
+    }
+    
+    public static function getFilters($filters){
+        $sql ='';
+        if(count($filters) > 0){
+            $sql .= "WHERE 1 = 1";
+            foreach($filters as $column => $value){
+                $sql .="AND  ${column} = ". static::getFormatedValue($value);
+            }
+        }
+    }
+    public static function getFormatedValue($value){
+        if(is_null($value)){
+            return "null";
+        }else if(gettype($value) === 'string'){
+            return "'{$value}'";
+        }else{
+            return $value;
+        }
+    }
+}
