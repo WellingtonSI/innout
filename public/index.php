062e601fb8c9c6b58f3414f715ad3d363dbcc599
wellingtonsi <12ribe@gmail.com>
2022-10-11 10:05:19 -0300

    model e tela de login

--- public/index.php
+++ public/index.php
@@ -1,17 +1,5 @@
 <?php
 
 require_once(dirname(__FILE__,2).'/src/config/config.php');
-require_once(dirname(__FILE__,2).'/src/models/User.php');
+require_once(dirname(__FILE__,2).'/src/views/login.php');
 
-$user = new User(['name'=>'mateus','email'=>'mateus@gmail.com']);
-
-print_r($user->email);
-//echo "FIm!";
-
-// $sql = 'select * from users';
-// $result = Database::getResultFromQuery($sql);
-
-// while($row = $result->fetch_assoc()){
-//     print_r($row);
-//     echo '<br>';
-// }
