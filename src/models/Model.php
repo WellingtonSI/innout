3ae48aca119a419dfe5dbb5903131a59ae649844
wellingtonsi <12ribe@gmail.com>
2022-11-09 17:13:55 -0300

    working hours

--- src/models/Model.php
+++ src/models/Model.php
@@ -56,6 +56,18 @@ class Model {
         }
         
     }
+
+    public function save(){
+        $sql = "INSERT INTO " . static::$tableName . " ("
+            . implode(",", static::$columns) . ") VALUES (";
+
+            foreach(static::$columns as $col){
+                $sql .= static::getFormatedValue($this->$col) . ",";
+            }
+            $sql[strlen($sql) - 1] = ')';
+            $id = DataBase::executeSQL($sql);
+            $this->id = $id;
+    }
     
     public static function getFilters($filters){
         $sql ='';
